270c5fb96b293792513f4d58d94b55c6b70dafd7
simo026q <simonchristensen03@gmail.com>
2022-02-19 17:04:29 +0100

    Lap logger concept and testing

---
+++ irsdkWrapper.Libary/Extensions/LapLogger/LapLogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using irsdkSharp.Serialization.Models.Data;
+
+namespace irsdkWrapper.Extensions.LapLogger
+{
+    interface ILapLogger<T>
+    {
+        public event EventHandler<LapLoggerEventArgs<T>> LapCompleted;
+        public void UpdateTelemetry(IRacingDataModel telemetry);
+    }
+
+    public class LapLoggerEventArgs<T> : EventArgs
+    {
+        public int DriverIdx { get; private set; }
+
+        public T CarInfo { get; private set; }
+
+        public LapLoggerEventArgs(T carInfo, int driverIdx)
+        {
+            DriverIdx = driverIdx;
+            CarInfo = carInfo;
+        }
+    }
+}
---
+++ irsdkWrapper.Libary/Extensions/LapLogger/Player/PlayerLapLogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using irsdkWrapper.Extensions.LapLogger;
+using irsdkSharp.Serialization.Models.Data;
+
+namespace irsdkWrapper.Extensions.LapLogger.Player
+{
+    public class PlayerLapLogger : ILapLogger<PlayerLapInfo>
+    {
+        public event EventHandler<LapLoggerEventArgs<PlayerLapInfo>> LapCompleted;
+
+        public void UpdateTelemetry(IRacingDataModel telemetry)
+        {
+
+        }
+    }
+}
