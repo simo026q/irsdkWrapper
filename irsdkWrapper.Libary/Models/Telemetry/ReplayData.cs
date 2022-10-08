40b5708b9d5d05897864d2deb23505e59c5791a8
simo026q <simonchristensen03@gmail.com>
2022-10-09 00:13:13 +0200

    Version 0.2.0

---
+++ irsdkWrapper.Libary/Models/Telemetry/ReplayData.cs
@@ -0,0 +1,41 @@
+﻿using irsdkWrapper.Enums;
+using irsdkSharp.Enums;
+using irsdkSharp.Serialization.Enums.Fastest;
+using irsdkSharp.Serialization.Models.Data;
+
+namespace irsdkWrapper.Models.Telemetry
+{
+    public class ReplayData
+    {
+        private IRacingDataModel _telemetry;
+
+        private DataModel _data => _telemetry.Data;
+
+        public ReplayData(IRacingDataModel telemetry)
+        {
+            _telemetry = telemetry;
+        }
+
+        public bool IsReplayPlaying => _data.IsReplayPlaying;
+
+        public int CamCameraNumber => _data.CamCameraNumber;
+
+        public CameraState CamCameraState => (CameraState)_data.CamCameraState;
+
+        public int CamCarIndex => _data.CamCarIdx;
+
+        public int CamGroupNumber => _data.CamGroupNumber;
+
+        public int ReplayFrameNumber => _data.ReplayFrameNum;
+
+        public int ReplayFrameNumberEnd => _data.ReplayFrameNumEnd;
+
+        public bool ReplayPlaySlowMotion => _data.ReplayPlaySlowMotion;
+
+        public int ReplayPlaySpeed => _data.ReplayPlaySpeed;
+
+        public int ReplaySessionNumber => _data.ReplaySessionNum;
+
+        public double ReplaySessionTime => _data.ReplaySessionTime;
+    }
+}
